a39fd2a57f7c78279a057966c461e36b66f24ee7
dnfwlxo11 <dnfwlxo11@naver.com>
2022-08-16 22:36:36 +0900

    edit: train-server docker setting

--- docker-env/flask/entrypoint.sh
+++ docker-env/flask/entrypoint.sh
@@ -13,6 +13,9 @@ git pull origin ${BRANCH_NAME}
 
 echo "git ${BRANCH_NAME} pull completed"
 
+echo -e \nalias python="python3" >> ~/.bash_profile
+source ~/.bash_profile
+
 celery multi start -A asyncFlask/job worker --loglevel=debug --logfile="./%n%I.log" --pidfile="./%n.pid" --autoscale=2,1 --max-tasks-per-child=1
 
-python3 app.py
+python app.py
