1357870cea68e1e2b3a832b463723eb0f1542d16
dnfwlxo11 <dnfwlxo11@naver.com>
2022-08-16 22:31:21 +0900

    edit: train-server docker setting

--- docker-env/flask/entrypoint.sh
+++ docker-env/flask/entrypoint.sh
@@ -13,8 +13,6 @@ git pull origin ${BRANCH_NAME}
 
 echo "git ${BRANCH_NAME} pull completed"
 
-echo -e ./bash_profile >> \nalias python="python3"
-
 celery multi start -A asyncFlask/job worker --loglevel=debug --logfile="./%n%I.log" --pidfile="./%n.pid" --autoscale=2,1 --max-tasks-per-child=1
 
-python app.py
+python3 app.py
--- train_server/app.py
+++ train_server/app.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 from flask import Flask, request, send_file, render_template, current_app
 import inference
 import base64
