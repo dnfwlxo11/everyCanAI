3a16c71cba03e58b521f5021f46a2370c8fa51b9
dnfwlxo11 <dnfwlxo11@naver.com>
2022-08-21 22:52:01 +0900

    edit: app server running setting change

--- app_server/app.js
+++ app_server/app.js
@@ -28,18 +28,23 @@ app.use('/node/models', require('./routes/model'));
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  next(createError(404));
-});
+  next(createError(404))
+})
 
-// error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use((err, req, res, next) => {
+  const msg = []
+  msg.push('\n======================= ERROR =======================')
+  msg.push(`Tracing: ${err.stack}`)
+  msg.push(`Message: ${err.toString()}`)
+  msg.push('======================= ERROR =======================\n')
+  console.error(msg.join('\n'))
+  res.status(err.status || 500).end()  
+})
 
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
+const server = app.listen(process.env.SERVER_PORT || 80, ()=>{
+  const host = server.address().address
+  const port = server.address().port
+  console.info(`App Listening at http://${host}:${port}`)
+})
 
 module.exports = app;
